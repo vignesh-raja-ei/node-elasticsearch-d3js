33d181dcaad60547d67f3f5bc39b08c33ab8a69b
vigneshraja-g <vigneshraja20@gmail.com>
2017-10-01 19:45:07 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express')
+var app = express()
+var path = require('path')
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  res.sendFile(path.join(__dirname + '/public/index.html'))
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
